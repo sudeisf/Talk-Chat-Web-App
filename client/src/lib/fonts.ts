06e9e18f2eb253ea4759a173afbfcd1c709588dd
sudeis <sudeisfed@gmail.com>
2025-10-21 01:28:46 +0300

    Refactor font management and update global styles for improved typography
    - Replace direct import of Inter font with a centralized font management approach using fontVariables.
    - Update global CSS to ensure consistent font usage across the application.
    - Clean up whitespace and enhance readability in layout component.

---
+++ client/src/lib/fonts.ts
@@ -0,0 +1,19 @@
+import {
+    Inter
+} from "next/font/google";
+
+
+export const inter = Inter({
+    subsets : ['latin'],
+    variable : '--font-inter',
+    display : 'swap'
+});
+
+
+export const  fontVariables = [
+    inter.variable
+].join(' ');
+
+export const fontClassName = {
+    inter : inter.className
+}
